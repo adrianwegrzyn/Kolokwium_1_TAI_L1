da373bee021f7ebe14195b5bc1aa560893f1e09c
Adrian <wegrzyn.adr@gmail.com>
2019-03-22 15:47:40 +0100

    kolokwium

---
+++ navBar.js
@@ -0,0 +1,7 @@
+let nav = '<h1>Navigacja</h1>\n' +
+    '<a href="index.html">Home</a>\n' +
+    '<a href="kontakt.html">Kontakt</a>';
+
+let navBar = document.querySelector('.nav-bar');
+
+navBar.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,6 @@
+let divColor = document.querySelector(".box");
+let button = document.querySelector(".button");
+
+button.addEventListener('click', ()=>{
+    divColor.className = 'boxRed';
+});
